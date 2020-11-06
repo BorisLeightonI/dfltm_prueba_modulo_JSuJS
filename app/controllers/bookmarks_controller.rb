e169f60e4de6eb75a88cc82435c28d598a6cc83b
Boris <borisleighton@gmail.com>
2020-11-06 17:33:08 -0300

    ultimo de la tarde, vuelvo al rato

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -32,6 +32,7 @@ class BookmarksController < ApplicationController
       if @bookmark.save
         format.html { redirect_to @bookmark, notice: 'Bookmark was successfully created.' }
         format.json { render :show, status: :created, location: @bookmark }
+        format.js {@bookmark}
       else
         format.html { render :new }
         format.json { render json: @bookmark.errors, status: :unprocessable_entity }
--- app/views/bookmarks/_bookmark.json.jbuilder
+++ app/views/bookmarks/_bookmark.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! bookmark, :id, :name, :type_id, :category_id, :created_at, :updated_at
+json.extract! bookmark, :id, :name, :type_id, :category_ids, :created_at, :updated_at
 json.url bookmark_url(bookmark, format: :json)
